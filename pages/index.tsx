35b66fa49ee0c955ef1efbebe34d01a19967c122
Alif <alifaziz@gmail.com>
2022-10-15 16:08:14 +0800

    Update reference

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import Quote from "../components/quote";
+import Quote from "../components/Quote";
 
 const Home: NextPage = () => {
   return (
