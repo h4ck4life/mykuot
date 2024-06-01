bc428ad47408109384a6611d15c7b563505f1551
Alif <alifaziz@gmail.com>
2024-06-01 11:17:01 +0800

    chore: Update Quote component and global styles

--- components/Quote.tsx
+++ components/Quote.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Quote } from "../types/Quote";
+import { Quote as QuoteType } from "../types/Quote";
 import PreLoader from "./PreLoader";
 
 type Props = {
@@ -7,16 +7,16 @@ type Props = {
 };
 
 const Quote = () => {
-  const [results, setResults] = useState({} as Quote);
+  const [results, setResults] = useState({} as QuoteType);
   const [isLoaded, setIsLoaded] = useState(false);
 
   const getQuote = async () => {
     const quoteResponse = await fetch(`https://api.quotable.io/random`);
     if (quoteResponse.ok) {
-      const data: Quote = await quoteResponse.json();
+      const data: QuoteType = await quoteResponse.json();
       setResults(data);
     } else {
-      alert('Something is wrong, try again later.');
+      alert("Something is wrong, try again later.");
     }
     setIsLoaded(true);
   };
@@ -25,10 +25,20 @@ const Quote = () => {
     if (isLoaded) {
       return (
         <div className="flex flex-col p-8 max-w-2xl transition-opacity duration-300 ease-in-out opacity-100">
-          <span className="my-2 text-4xl drop-shadow-sm">
+          <span className="my-2 text-2xl sm:text-4xl drop-shadow-sm">
             {results.content}
           </span>
           <span className="my-4 drop-shadow-sm italic">{results.author}</span>
+          <footer className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-xs text-gray-600">
+            Made by{" "}
+            <a
+              href="https://x.com/h4ck4life"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @h4ck4life
+            </a>
+          </footer>
         </div>
       );
     } else {
